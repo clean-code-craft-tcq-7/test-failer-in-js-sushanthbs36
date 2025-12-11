9742b472be9a0403de63fdc7bb8658a0dc1ab8ea
Sushanth B S <sushanth.bs@numocity.com>
2025-12-11 10:27:05 +0530

    Seperation of Test Code in Weather Report File

--- test_weatherreport.mjs
+++ test_weatherreport.mjs
@@ -1,10 +1,17 @@
 import {expect} from 'chai'
-import { highPrecipitationStub, report } from './weatherreport.mjs'
+import {report } from './weatherreport.mjs'
 
-//Test for Weather Report
+function sensorStub() {
+    return {
+        temperatureInC: 30,
+        precipitation: 80,
+        humidity: 26,
+        windSpeedKMPH: 20
+    };
+}
 describe('Weather Reporting', () => {
     it('Should alert for high precipitation (>60) even with low wind as High Precipitation', () => {
-        const weatherReport=report(highPrecipitationStub)
+        const weatherReport=report(sensorStub)
         expect(weatherReport).to.equal("High Precipitation")
     })
 })
--- weatherreport.mjs
+++ weatherreport.mjs
@@ -1,26 +1,3 @@
-import { expect } from 'chai';
-
-export function sensorStub() {
-    return {
-        temperatureInC: 50,
-        precipitation: 70,
-        humidity: 26,
-        windSpeedKMPH: 52
-    };
-}
-export function highPrecipitationStub() {
-    return { 
-        temperatureInC: 30,         
-        precipitation: 80,          
-        humidity: 26, 
-        windSpeedKMPH: 20          
-    };
-}
-export function testRainy() {
-    const weather = report(sensorStub);
-    console.log(weather);
-    expect(weather).includes("rain", "Test failed: Expected 'rain' in weather description");
-}
 export function report(sensorReader) {
     const readings = sensorReader();
     let weather = "Sunny Day";
